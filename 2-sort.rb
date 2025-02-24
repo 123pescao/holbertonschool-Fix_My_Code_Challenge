1cd769760ab48ce2949e46ebb09aaa625f44ef12
Joel <joel.caban2017@gmail.com>
2025-02-24 11:10:06 -0400

    Tasks & Readme

---
+++ 2-sort.rb
@@ -0,0 +1,27 @@
+# Initialize an empty array to store sorted integers
+result = []
+
+# Iterate over each argument provided to the script
+ARGV.each do |arg|
+  # Skip if the argument is not an integer
+  next unless arg =~ /^-?\d+$/
+
+  # Convert the argument to an integer
+  i_arg = arg.to_i
+
+  # Insert the integer into the result array while maintaining order
+  inserted = false
+  result.each_with_index do |num, index|
+    if i_arg < num
+      result.insert(index, i_arg)
+      inserted = true
+      break
+    end
+  end
+
+  # If the integer is larger than all elements in the array, append it at the end
+  result.push(i_arg) unless inserted
+end
+
+# Print each element
+result.each { |num| puts num }
